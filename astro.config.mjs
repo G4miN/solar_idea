63acf16248c3a9308eaf37b5f03c9c2c8375020a
Yeduin Mateo <el_yeduin_02@hotmail.com>
2025-11-17 13:00:56 -0400

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 // @ts-check
 
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
-
+import { defineConfig, fontProviders } from 'astro/config';
 import react from '@astrojs/react';
 
 // https://astro.build/config
@@ -12,4 +11,12 @@ export default defineConfig({
 	},
 
   integrations: [react()],
+
+  experimental: {
+    fonts: [{
+      provider: fontProviders.fontsource(),
+      name:"Varela Round",
+      cssVariable: "--font-varela-round"
+    }]
+  }
 });
